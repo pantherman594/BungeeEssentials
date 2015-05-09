455f7a3a6a140d1c39d5d2f21597bed8e0728471
PantherMan594 <eiasddhvn@gmail.com>
2015-05-09 18:51:09 -0400

    Added announcements On a timed interval

---
+++ src/main/java/de/albionco/gssentials/announcement/Anncs.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Connor Spencer Harries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.albionco.gssentials.announcement;
+
+import com.google.common.base.Preconditions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by David on 5/8/2015.
+ *
+ * @author David Shen
+ */
+public class Anncs {
+    public static HashMap<Integer, Integer> annc_Interval = new HashMap<>();
+    public static HashMap<Integer, String> annc_Msg = new HashMap<>();
+
+    public static void deserialize(Map<String, String> serialized) {
+        int size = annc_Interval.size();
+        Preconditions.checkNotNull(serialized);
+        Preconditions.checkArgument(!serialized.isEmpty());
+        Preconditions.checkNotNull(serialized.get("interval"), "invalid interval");
+        Preconditions.checkNotNull(serialized.get("message"), "invalid message");
+
+        size++;
+        annc_Interval.put(size, Integer.parseInt(String.valueOf(serialized.get("interval"))));
+        annc_Msg.put(size, String.valueOf(serialized.get("message")));
+    }
+}
---
+++ src/main/java/de/albionco/gssentials/announcement/AnncsManager.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2015 Connor Spencer Harries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.albionco.gssentials.announcement;
+
+import de.albionco.gssentials.BungeeEssentials;
+import de.albionco.gssentials.Dictionary;
+import net.md_5.bungee.api.ProxyServer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+/**
+ * Created by David on 5/8/2015.
+ *
+ * @author David Shen
+ */
+@SuppressWarnings("deprecation")
+public class AnncsManager {
+    private static List<Anncs> anncs = new ArrayList<>();
+
+    @SuppressWarnings("unchecked")
+    public static boolean load() {
+        Anncs.annc_Interval = new HashMap<>();
+        Anncs.annc_Msg = new HashMap<>();
+        anncs.clear();
+        List<Map<String, String>> section = (List<Map<String, String>>) BungeeEssentials.getInstance().getConfig().getList("announcements");
+        for (Map<String, String> map : section) {
+            Anncs.deserialize(map);
+        }
+        int success = Anncs.annc_Interval.size();
+        if (success > 0) {
+            BungeeEssentials.getInstance().getLogger().log(Level.INFO, "Loaded {0} announcements from config", success);
+            scheduleAnnc();
+        }
+        return true;
+    }
+
+    private static void scheduleAnnc() {
+        for (final Integer num : Anncs.annc_Interval.keySet()) {
+            final int interval = Anncs.annc_Interval.get(num);
+            final String msg = Anncs.annc_Msg.get(num);
+            ProxyServer.getInstance().getScheduler().schedule(BungeeEssentials.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    ProxyServer.getInstance().broadcast(Dictionary.format(Dictionary.FORMAT_ALERT, "MESSAGE", msg));
+                    scheduleAnnc(num, interval, msg);
+                }
+            }, interval, TimeUnit.SECONDS);
+        }
+    }
+
+    private static void scheduleAnnc(final Integer num, final Integer interval, final String msg) {
+        if (interval == Anncs.annc_Interval.get(num) && msg.equals(Anncs.annc_Msg.get(num))) {
+            ProxyServer.getInstance().getScheduler().schedule(BungeeEssentials.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    ProxyServer.getInstance().broadcast(Dictionary.format(Dictionary.FORMAT_ALERT, "MESSAGE", msg));
+                    scheduleAnnc(num, interval, msg);
+                }
+            }, interval, TimeUnit.SECONDS);
+        }
+    }
+}
