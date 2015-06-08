26766f9ab5d3cddd142a9b6b3d6381fdf8c0f29e
PantherMan594 <eiasddhvn@gmail.com>
2015-06-08 01:01:18 -0400

    Finish Testing

--- src/main/java/de/albionco/gssentials/event/PlayerListener.java
+++ src/main/java/de/albionco/gssentials/event/PlayerListener.java
@@ -134,16 +134,6 @@ public class PlayerListener implements Listener {
                 }
                 if (currPlayers != connections.get(address)) {
                     connections.put(address, currPlayers);
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
-                    BungeeEssentials.getInstance().getLogger().log(Level.INFO, "test!");
                 }
                 int newCount = connections.get(address) + 1;
                 if (newCount > max) {
